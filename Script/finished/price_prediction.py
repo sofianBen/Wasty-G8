94f4dbde0f70e47c3f125e57c2fddb4dfa2b271a
GiovanniZanitti <giovanni.zanitti@gmail.com>
2017-01-11 13:34:54 +0100

    Ajout du programme permettant de predire le prix et deplacement du jeu de données "Annonce.json"

---
+++ Script/finished/price_prediction.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 11 10:34:04 2017
+
+@author: Giovanni
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 11 08:43:37 2017
+
+@author: Giovanni
+"""
+
+## --------------------------------------------------------
+# Projet : WASTY DataBase
+# Groupe 8 : Prédiction et recommandation
+# Objectif : La prédiction des prix
+# Par : Giovanni Zanitti - CHRISMANN Céline - QUESNOT Sandy
+## --------------------------------------------------------
+import json
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import preprocessing
+
+# Ouverture de la table Annonce 
+# Import des donnees au format json
+with open('../../data/Advert/Annonce.json') as data_file:    
+    data = json.load(data_file)
+
+# Transformation des donnees en dataframe
+df = pd.read_json(data)
+# Suppression des lignes ou le prix n'est pas renseigne
+df = df[df['forecast_price'] != '']
+
+
+# Variable a renseigner dans la requete
+var_req = ['buy_place',
+           'id_sub_category',
+           'object_state',
+           'quantite',
+           'type_place',
+           'volume']
+
+
+def price_prediction(pu_req):
+    # Mise en forme de la requete et suppression des champs a valeur
+    # manquante
+    X_pred = pd.DataFrame(pu_req).T
+    X_pred.columns = var_req
+    X_pred = X_pred[X_pred != ''].T.dropna().T
+
+    # Variable necessaire pour entrainer le modele
+    var_X_train = list(X_pred.columns)
+
+    # Donnees pour entrainer le modele
+    X_train = df[var_X_train]
+
+    # Encodage des donnees en facteur numeriques
+    le = preprocessing.LabelEncoder()
+
+    # Pour toutes les variables necessaire
+    for var in var_X_train:
+        # On assigne les strings a des integers
+        le.fit(X_train[var])
+        # On les transforme dans le X_train
+        X_train[var] = list(le.transform(X_train[var]))
+        # Et dans le X_pred
+        X_pred[var] = list(le.transform(X_pred[var]))
+
+    # Donnees d'entrainement a predire
+    Y_train = df['forecast_price']
+    # Conversion des donnees en numerique
+    Y_train = Y_train.convert_objects(convert_numeric=True)
+
+    # Modele utilise : RandomForest
+    rf = RandomForestRegressor(n_estimators=1000)
+    # Entrainement du modele avec les donnees adequats
+    rf.fit(X_train, Y_train)
+    # Prediction
+    return rf.predict(X_pred).item()
